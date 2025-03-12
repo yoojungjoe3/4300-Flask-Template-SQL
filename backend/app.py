2c6ba07cb10f88a2117d664798dc29a2f7ba2b3f
Yoojung Joe <142810755+yoojungjoe3@users.noreply.github.com>
2025-03-12 21:02:09 +0000

    updated many things

--- backend/app.py
+++ backend/app.py
@@ -12,9 +12,9 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 LOCAL_MYSQL_USER = "root"
-LOCAL_MYSQL_USER_PASSWORD = "admin"
+LOCAL_MYSQL_USER_PASSWORD = "quillquest"
 LOCAL_MYSQL_PORT = 3306
-LOCAL_MYSQL_DATABASE = "kardashiandb"
+LOCAL_MYSQL_DATABASE = "quillquestdb"
 
 mysql_engine = MySQLDatabaseHandler(LOCAL_MYSQL_USER,LOCAL_MYSQL_USER_PASSWORD,LOCAL_MYSQL_PORT,LOCAL_MYSQL_DATABASE)
 
