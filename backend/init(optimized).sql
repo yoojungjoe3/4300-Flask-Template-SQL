83fb04ecaea8ec5b845939902841c6d78dbb368e
Kamie <38571948+KAsqech@users.noreply.github.com>
2025-03-23 22:04:57 +0000

    Changed init.sql back to unoptimized version

---
+++ backend/init(optimized).sql
@@ -0,0 +1,8 @@
+
+LOAD DATA INFILE '/var/lib/mysql-files/DataSet_clean.csv'
+INTO TABLE fanfics
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Name, Fandom, Ships, Rating, Link, Review, Abstract);
