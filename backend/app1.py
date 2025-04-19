5b29687e1865f3b540bd3ff2fc17479c73ea7cd7
Kamie <38571948+KAsqech@users.noreply.github.com>
2025-04-19 16:31:21 +0000

    Changed db credentials

--- backend/app1.py
+++ backend/app1.py
@@ -18,13 +18,14 @@ from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # Database credentials (adjust if needed)
-LOCAL_MYSQL_USER = "root"
-LOCAL_MYSQL_USER_PASSWORD = "bobbob"
-LOCAL_MYSQL_PORT = 3306
-LOCAL_MYSQL_DATABASE = "kardashiandb"
+MYSQL_HOST = "quill_quest_db"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "bobbob"
+MYSQL_PORT = 3306
+MYSQL_DB = "kardashiandb"
 
 # Initialize database handler and load init.sql into the database
-mysql_engine = MySQLDatabaseHandler(LOCAL_MYSQL_USER, LOCAL_MYSQL_USER_PASSWORD, LOCAL_MYSQL_PORT, LOCAL_MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(MYSQL_USER, MYSQL_PASSWORD, MYSQL_PORT, MYSQL_DB, MYSQL_HOST)
 mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
